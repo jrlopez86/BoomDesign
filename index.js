671b5864f3d4f8ec7a239fa5c14ce36a7256e311
Rafael <jrlopez.16286@gmail.com>
2023-03-19 20:21:59 +0100

    se modifico la web con los requisitos del cliente

---
+++ index.js
@@ -0,0 +1,22 @@
+// import { annotate, annotationGroup } from 'https://unpkg.com/rough-notation?module';
+
+// const a = document.getElementById('prueba1')
+// const b = document.getElementById('prueba2')
+// const c = document.getElementById('prueba3')
+// const d = document.getElementById('prueba4')
+
+
+
+// const a1 = annotate(a, {type: 'box', color: 'black'})
+// const a2 = annotate(b, {type: 'box', color: 'black'})
+// const a3 = annotate(c, {type: 'box', color: 'black'})
+// const a4 = annotate(d, {type: 'box', color: 'black'})
+
+
+
+
+// a1.show()
+// a2.show()
+// a3.show()
+// a4.show()
+
